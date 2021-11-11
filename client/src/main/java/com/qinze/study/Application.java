aff8ec32b019f635f18ca2459f71dd65efc5a8f7
qinze <qinze@tal.com>
2021-11-11 23:21:50 +0800

    项目初始化

--- client/src/main/java/com/qinze/study/Application.java
+++ client/src/main/java/com/qinze/study/Application.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.qinze.study")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
