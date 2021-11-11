db7f388c6423c094cd2a0e2287f4a5fbcac41b5e
qinze <qinze@tal.com>
2021-11-11 22:01:30 +0800

    初始化

---
+++ client/src/main/java/com/qinze/study/Application.java
@@ -0,0 +1,11 @@
+package com.qinze.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.qinze.study")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
